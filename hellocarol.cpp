46c7740a73793c77150604a1b90a3a493efbb4b9
iury-igd1 <iurygoncalvessrs1@gmail.com>
2026-04-08 10:46:56 -0300

    cloneCarol

---
+++ hellocarol.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "pt_BR.utf8");
+
+    cout << "\033[1;34m***************************************************\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m Oii Carol! Tudo bem?               \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mClonei seu repositório!                       \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mAceita meu request aí viu                     \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*                                                 *\033[0m\n";
+    cout << "\033[1;34m***************************************************\033[0m\n";
+
+    return 0;
+}
