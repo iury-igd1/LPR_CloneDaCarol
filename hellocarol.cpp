1e5f1b9ae767f7bca229f6ce2c87fbaeedd7a3c3
iury-igd1 <iurygoncalvessrs1@gmail.com>
2026-04-14 13:38:40 -0300

    commitNovo

--- hellocarol.cpp
+++ hellocarol.cpp
@@ -11,7 +11,7 @@ int main() {
     cout << "\033[1;32m*   \033[1;33m Oii Carol! Tudo bem?               \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mClonei seu repositório!                       \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mAceita meu request aí viu                     \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mAceita meu request aí ta                      \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
 
